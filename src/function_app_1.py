e869c1017fbcd4d1eec53c5bba97784e3a7809f3
arulrajgopal <arulrajgopal@oulook.com>
2026-03-05 19:08:13 +0530

    update

--- src/function_app_1.py
+++ src/function_app_1.py
@@ -3,7 +3,7 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="hello", auth_level=func.AuthLevel.FUNCTION)
+@app.route(route="hello1", auth_level=func.AuthLevel.FUNCTION)
 def hello(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Processing request...")
 
